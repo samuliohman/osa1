989b69ecf100fa231783ddab0f3ff8e282790b5e
Samuli Öhman <samuli.ohman@aalto.fi>
2023-01-29 15:55:01 +0200

    Tehtävä 1.8

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>Average {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive {good / (good + neutral + bad)}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -12,12 +25,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}> Good </button>
       <button onClick={() => setNeutral(neutral + 1)}> Neutral </button>
       <button onClick={() => setBad(bad + 1)}> Bad </button>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive {good / (good + neutral + bad)}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
